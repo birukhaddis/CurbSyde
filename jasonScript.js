636afaf17a1b6b95a36e4831a094f869c8a984b1
Biruk <49503630+birukhaddis@users.noreply.github.com>
2020-11-27 14:12:39 -0800

    Add files via upload

---
+++ jasonScript.js
@@ -0,0 +1,34 @@
+// open links in a new tab
+
+function openHome() {
+  window.open("index.html", "_blank");
+}
+
+function openCDC() {
+  window.open("https://www.cdc.gov/coronavirus/2019-ncov/prevent-getting-sick/prevention.html?CDC_AA_refVal=https%3A%2F%2Fwww.cdc.gov%2Fcoronavirus%2F2019-ncov%2Fprepare%2Fprevention.html", "_blank");
+}
+
+function openRestaurants() {
+  window.open("https://www.google.com/maps/search/restaurants+near+me/@47.6217047,-122.1507573,13z/data=!3m1!4b1", "_blank");
+}
+
+function openGrocery() {
+  window.open("https://www.google.com/maps/search/grocery+near+me/@47.621694,-122.150929,13z/data=!3m1!4b1", "_blank");
+}
+
+function openAuto() {
+  window.open("https://www.google.com/maps/search/mechanic+near+ME/@47.6216834,-122.1511007,13z/data=!3m1!4b1", "_blank");
+
+}
+
+function openRoadside() {
+  window.open("https://www.google.com/maps/search/auto+repair+near+me/@47.621726,-122.150414,13z/data=!3m1!4b1", "_blank");
+}
+
+function openPet() {
+  window.open("https://www.google.com/maps/search/pet+shop+near+me/@47.6217153,-122.1505856,13z/data=!3m1!4b1", "_blank");
+}
+
+function openOptions() {
+  window.open("https://www.google.com/maps/search/popular+near+me/@47.6216727,-122.1512723,13z/data=!3m1!4b1", "_blank");
+}
